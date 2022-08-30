e7ba24b2dfe5216568f2842929a3426b65065e43
dhmk47 <dhmk47@naver.com>
2022-08-30 20:11:10 +0900

    팀프로젝트 시작

---
+++ src/main/java/com/project/winiaaid/WiniaaidApplication.java
@@ -0,0 +1,13 @@
+package com.project.winiaaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiniaaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiniaaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/winiaaid/WiniaaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.winiaaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiniaaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
