b314786ea7b5275c8999b5b3832d6135b51f2e68
dhmk47 <dhmk47@naver.com>
2022-09-25 01:29:55 +0900

    no message

--- src/main/java/com/project/winiaaid/domain/user/UserRepository.java
+++ src/main/java/com/project/winiaaid/domain/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.project.winiaaid.domain.user;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface UserRepository {
