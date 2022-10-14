e90f23d127043ade821abe8b74c6dc5b650a8e62
dhmk47 <dhmk47@naver.com>
2022-10-14 20:47:29 +0900

    5차 배포 시도

--- src/main/resources/static/js/popup/popup.js
+++ src/main/resources/static/js/popup/popup.js
@@ -1,20 +1,20 @@
 function showModelNumberCheckPopup() {
-    window.open("http://localhost:8000/service/popup/model-name-popup", "modelCheckPopup", 
+    window.open("https://winiaaid-daegyeong.herokuapp.com/service/popup/model-name-popup", "modelCheckPopup", 
     "width=700, height=800");
 }
 
 function loadAddressPopup() {
-    window.open("http://localhost:8000/address/main", "setAddressPopup", 
+    window.open("https://winiaaid-daegyeong.herokuapp.com/address/main", "setAddressPopup", 
     "width=700, height=800");
 }
 
 function loadPastRequestInfoPopup() {
-    window.open("http://localhost:8000/service/popup/past-request-popup", "setAddressPopup", 
+    window.open("https://winiaaid-daegyeong.herokuapp.com/service/popup/past-request-popup", "setAddressPopup", 
     "width=700, height=800");
 }
 
 function loadPastAddressListInfoPopup() {
-    window.open("http://localhost:8000/service/popup/past-address-popup", "setAddressPopup", 
+    window.open("https://winiaaid-daegyeong.herokuapp.com/service/popup/past-address-popup", "setAddressPopup", 
     "width=700, height=800");
 }
 
